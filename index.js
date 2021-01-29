6d3ebb6965d31813e3aab1c416bdbaeb6580ea5a
Ryan Welcher <ryan.welcher@10up.com>
2021-01-29 09:14:21 -0500

    Add CustomBlockAppender.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { ContentPicker } from './components/content-picker';
 export { IsAdmin } from './components/is-admin';
 export { useHasSelectedInnerBlock } from './hooks/use-has-selected-inner-block';
+export { default as CustomBlockAppender } from './components/CustomBlockAppender';
