2440f27be5cacd9a6b1ae61ba546085efc785daa
Alejandro Perez Martin <aleperez92@gmail.com>
2021-08-12 10:40:32 +0200

    Keep drag handle icon size

--- components/ContentPicker/PickedItem.js
+++ components/ContentPicker/PickedItem.js
@@ -16,7 +16,7 @@ import { useEffect } from '@wordpress/element';
 
 const DragHandle = sortableHandle(() => (
 	<svg
-		style={{ marginRight: '5px', cursor: 'grab' }}
+		style={{ marginRight: '5px', cursor: 'grab', flexShrink: 0 }}
 		width="18"
 		height="18"
 		xmlns="http://www.w3.org/2000/svg"
