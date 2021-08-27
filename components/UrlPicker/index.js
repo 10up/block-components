5416bfec33a65839d3d9e089764edefe39af6e54
Helen Hou-Sandi <helen.y.hou@gmail.com>
2021-08-27 13:38:42 -0600

    Add UrlPicker component

---
+++ components/UrlPicker/index.js
@@ -0,0 +1,106 @@
+/**
+ * WordPress dependencies
+ */
+ import { __ } from '@wordpress/i18n';
+ import { useState } from '@wordpress/element';
+ import {
+   KeyboardShortcuts,
+   Popover,
+   Button,
+   Tooltip,
+ } from '@wordpress/components';
+ import {
+   PlainText,
+   __experimentalLinkControl as LinkControl,
+ } from '@wordpress/block-editor';
+ import { rawShortcut } from '@wordpress/keycodes';
+ import { debounce } from 'lodash';
+ import { searchablePostTypes } from './helper';
+ 
+ export default function UrlPicker({
+   url,
+   opensInNewTab,
+   onLinkChange,
+   onTextChange,
+   placeholder,
+   text,
+   isBlockSelected,
+   title,
+   linkOptional,
+   ...extraProps
+ }) {
+   const [isURLPickerOpen, setIsURLPickerOpen] = useState(false);
+   const [selected, setSelected] = useState(false);
+   const openLinkControl = () => {
+	 setIsURLPickerOpen(true);
+	 return false; // prevents default behaviour for event
+   };
+ 
+   const linkControl = isURLPickerOpen && (
+	 <Popover
+	   position="bottom center"
+	   onClose={() => {
+		 setIsURLPickerOpen(false);
+		 setSelected(false);
+	   }}
+	 >
+	   {title && <h4 className="tenup-popover-title">{title}</h4>}
+	   <LinkControl
+		 className="wp-block-navigation-link__inline-link-input"
+		 value={{ url, opensInNewTab }}
+		 onChange={onLinkChange}
+		 suggestionsQuery={{
+		   subtype: searchablePostTypes.join(','),
+		 }}
+	   />
+	 </Popover>
+   );
+   return (
+	 <div className="tenup-url-picker">
+	   {selected && (
+		 <KeyboardShortcuts
+		   bindGlobal
+		   shortcuts={{
+			 [rawShortcut.primary('k')]: openLinkControl,
+		   }}
+		 />
+	   )}
+	   {linkControl}
+	   <PlainText
+		 value={text}
+		 onChange={(value) => {
+		   onTextChange(value.replace(/[\r\n\t]+/gm, ' '));
+		 }}
+		 placeholder={placeholder}
+		 keepPlaceholderOnFocus
+		 onFocus={debounce(() => {
+		   setSelected(true);
+		 }, 100)}
+		 onBlur={debounce(() => {
+		   setSelected(false);
+		 }, 100)}
+		 {...extraProps}
+	   />
+ 
+	   <Button
+		 className={`
+		 link-toggle is-small
+		 ${selected && 'active'}
+		 ${isBlockSelected || 'hidden'}
+		 `}
+		 icon={<span className="dashicons dashicons-admin-links"></span>}
+		 onClick={openLinkControl}
+	   />
+ 
+	   {!linkOptional && text && !url && (
+		 <Tooltip text={__('This item is missing a link', '10up-block-components')}>
+		   <span
+			 tabIndex={0}
+			 className="missing-link dashicons dashicons-warning"
+		   />
+		 </Tooltip>
+	   )}
+	 </div>
+   );
+ }
+ 
