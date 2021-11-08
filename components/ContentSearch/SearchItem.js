a740e56b360e0b1003bd96168e62a5849a963323
Alex Dimitrov <alex.dimitrov@10up.com>
2021-11-08 15:24:50 +0200

    SearchItem - Add tooltip on hover

--- components/ContentSearch/SearchItem.js
+++ components/ContentSearch/SearchItem.js
@@ -1,7 +1,24 @@
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import { safeDecodeURI, filterURLForDisplay } from '@wordpress/url';
 import { decodeEntities } from '@wordpress/html-entities';
-import { Button, TextHighlight } from '@wordpress/components';
+import { Button, TextHighlight, Tooltip } from '@wordpress/components';
+
+const ButtonStyled = styled(Button)`
+	&:hover {
+		/* Add opacity background to support future color changes */
+		/* Reduce background from #ddd to 0.05 for text contrast  */
+		background-color: rgba(0, 0, 0, 0.05);
+
+		.block-editor-link-control__search-item-type {
+			color: black;
+		}
+	}
+
+	.block-editor-link-control__search-item-type {
+		background-color: rgba(0, 0, 0, 0.05);
+	}
+`;
 
 /**
  * SearchItem
@@ -18,43 +35,45 @@ const SearchItem = ({ suggestion, onClick, searchTerm, isSelected, id, contentTy
 	const showType = suggestion.type && contentTypes.length > 1;
 
 	return (
-		<Button
-			id={id}
-			onClick={onClick}
-			className={`block-editor-link-control__search-item is-entity ${
-				isSelected && 'is-selected'
-			}`}
-			style={{
-				borderRadius: '0',
-				boxSizing: 'border-box',
-			}}
-		>
-			<span className="block-editor-link-control__search-item-header">
-				<span
-					className="block-editor-link-control__search-item-title"
-					style={{
-						paddingRight: !showType ? 0 : null,
-					}}
-				>
-					<TextHighlight text={decodeEntities(suggestion.title)} highlight={searchTerm} />
+		<Tooltip text={decodeEntities(suggestion.title)}>
+			<ButtonStyled
+				id={id}
+				onClick={onClick}
+				className={`block-editor-link-control__search-item is-entity ${
+					isSelected && 'is-selected'
+				}`}
+				style={{
+					borderRadius: '0',
+					boxSizing: 'border-box',
+				}}
+			>
+				<span className="block-editor-link-control__search-item-header">
+					<span
+						className="block-editor-link-control__search-item-title"
+						style={{
+							paddingRight: !showType ? 0 : null,
+						}}
+					>
+						<TextHighlight text={decodeEntities(suggestion.title)} highlight={searchTerm} />
+					</span>
+					<span
+						aria-hidden
+						className="block-editor-link-control__search-item-info"
+						style={{
+							paddingRight: !showType ? 0 : null,
+						}}
+					>
+						{filterURLForDisplay(safeDecodeURI(suggestion.url)) || ''}
+					</span>
 				</span>
-				<span
-					aria-hidden
-					className="block-editor-link-control__search-item-info"
-					style={{
-						paddingRight: !showType ? 0 : null,
-					}}
-				>
-					{filterURLForDisplay(safeDecodeURI(suggestion.url)) || ''}
-				</span>
-			</span>
-			{showType && (
-				<span className="block-editor-link-control__search-item-type">
-					{/* Rename 'post_tag' to 'tag'. Ideally, the API would return the localised CPT or taxonomy label. */}
-					{suggestion.type === 'post_tag' ? 'tag' : suggestion.type}
-				</span>
-			)}
-		</Button>
+				{showType && (
+					<span className="block-editor-link-control__search-item-type">
+						{/* Rename 'post_tag' to 'tag'. Ideally, the API would return the localised CPT or taxonomy label. */}
+						{suggestion.type === 'post_tag' ? 'tag' : suggestion.type}
+					</span>
+				)}
+			</ButtonStyled>
+		</Tooltip>
 	);
 };
 
