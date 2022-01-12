76b437380c091351f6053609df1bebd60ea09c1b
Fabian Kägy <mail@fabian-kaegy.de>
2022-01-12 13:19:45 +0100

    add new example block to showcase Link component

---
+++ example/src/blocks/link-example/edit.js
@@ -0,0 +1,39 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+import { Link } from '@10up/block-components';
+
+export function BlockEdit(props) {
+    const {
+        attributes,
+        setAttributes
+    } = props;
+
+    const { text, url, opensInNewTab } = attributes;
+
+    const blockProps = useBlockProps();
+
+    const handleTextChange = value => setAttributes({text: value});
+    const handleLinkChange = value => setAttributes({
+        url: value?.url,
+        opensInNewTab: value?.opensInNewTab,
+        text: value?.title ?? text
+    });
+
+    return (
+        <div {...blockProps}>
+            <h2>Hello World!</h2>
+            <div>
+                <Link 
+                    value={text}
+                    url={url}
+                    opensInNewTab={opensInNewTab}
+                    onTextChange={handleTextChange}
+                    onLinkChange={handleLinkChange}
+                    className='example-classname'
+                    placeholder='Enter Link Text here...'
+                />
+            </div>
+        </div>
+    )
+}
