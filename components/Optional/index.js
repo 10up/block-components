9126533d3f300e2688b38a62476ddea8ed31426b
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-01-17 16:57:08 +0530

    feat: add Optional Component

---
+++ components/Optional/index.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { useBlockEditContext } from '@wordpress/block-editor';
+
+export const Optional = ({ value, children }) => {
+	const { isSelected } = useBlockEditContext();
+	return !!value && isSelected && <>{children}</>;
+};
+
+Optional.defaultProps = {
+	value: '',
+};
+
+Optional.propTypes = {
+	value: PropTypes.string,
+	children: PropTypes.node.isRequired,
+};
