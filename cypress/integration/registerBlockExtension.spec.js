9d216e76bc55128d032dce569c24cceaeda4986d
Fabian Kägy <mail@fabian-kaegy.de>
2022-02-16 14:41:29 +0100

    also add test to ensure that custom class names still work

--- cypress/integration/registerBlockExtension.spec.js
+++ cypress/integration/registerBlockExtension.spec.js
@@ -2,7 +2,7 @@
 
 context('registerBlockExtension', () => {
 
-	before(() => {
+	beforeEach(() => {
 		cy.loginToWordPress();
 	});
 
@@ -21,6 +21,35 @@ context('registerBlockExtension', () => {
 
         // ensure the new class names get applied in the frontend
 		cy.get('.wp-block-group').should('have.class', 'has-background-pattern');
+
+        cy.contains('Edit Page').click();
+
+        // ensure the editor loads without eny deprecation errors
+        cy.get('.wp-block-group').contains('This block contains unexpected or invalid content.').should('not.exist');
+	})
+	
+    it('ensure the new settings still respect custom classnames', () => {
+		cy.createPost({title: 'Block Extension Test'});
+		cy.insertBlock('Group');
+        cy.contains('Show Background Pattern').click();
+
+        // ensure the new class names get applied in the editor
+        cy.get('.wp-block-group').should('have.class', 'has-background-pattern');
+        
+        cy.get('.components-panel__body-title').contains('Advanced').click();
+        
+        cy.contains('Additional CSS class(es)').next().type('test-class');
+        
+        cy.get('.wp-block-group').should('have.class', 'test-class');
+		
+		cy.savePost();
+
+		// click on the View Post snackbar item
+		cy.get('.components-snackbar a').click();
+
+        // ensure the new class names get applied in the frontend
+		cy.get('.wp-block-group').should('have.class', 'has-background-pattern');
+		cy.get('.wp-block-group').should('have.class', 'test-class');
 
         cy.contains('Edit Page').click();
 
