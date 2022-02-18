7180979c24602170d2ec798926b2419c23e26834
Fabian Kägy <mail@fabian-kaegy.de>
2022-02-18 17:14:13 +0100

    fix externalize wordpress packages as commonjs2 externals as to still get picked up by the DEWP in the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
 // eslint-disable-next-line import/extensions
 const config = require('10up-toolkit/config/webpack.config.js');
 
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
+const webpack = require('webpack');
+
+const WORDPRESS_NAMESPACE = '@wordpress/';
+const BUNDLED_PACKAGES = ['@wordpress/icons', '@wordpress/interface', '@wordpress/style-engine'];
+
+const requestToExternal = (request) => {
+	if (BUNDLED_PACKAGES.includes(request)) {
+		return undefined;
+	}
+
+	if (request.startsWith(WORDPRESS_NAMESPACE)) {
+		return request;
+	}
+
+	return undefined;
+};
+function externalizeWpDeps(_context, request, callback) {
+	const externalRequest = requestToExternal(request);
+
+	if (externalRequest) {
+		return callback(null, externalRequest, 'commonjs2');
+	}
+
+	return callback();
+}
+
+function externalizeWpDepsV5({ context, request }, callback) {
+	return externalizeWpDeps(context, request, callback);
+}
 
 module.exports = {
 	...config,
-	plugins: [
-		...config.plugins,
-		new DependencyExtractionWebpackPlugin({
-			requestToExternal: () => undefined,
-			requestToHandle: () => undefined,
-		}),
-	],
+	plugins: [...config.plugins, new webpack.ExternalsPlugin(null, externalizeWpDepsV5)],
 };
