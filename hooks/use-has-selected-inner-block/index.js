0ea5a412ecc51fa4d09f9f406a468fd3859aff49
Fabian Kägy <mail@fabian-kaegy.de>
2022-05-12 13:37:26 +0200

    fix update `useHasSelectedInnerBlock` hook to use BlockEditContext to get the clientID

--- hooks/use-has-selected-inner-block/index.js
+++ hooks/use-has-selected-inner-block/index.js
@@ -1,13 +1,14 @@
 import { useSelect } from '@wordpress/data';
+import { useBlockEditContext } from '@wordpress/block-editor';
 
 /**
  * useHasSelectedInnerBlock
  * Determine whether one of the inner blocks currently is selected
  *
- * @param {object} props react props
- * @param {string} props.clientId client id of the block
  * @returns {boolean} wether the block is the ancestor of selected blocks
  */
-export function useHasSelectedInnerBlock({ clientId }) {
+export function useHasSelectedInnerBlock() {
+	const { clientId } = useBlockEditContext();
+
 	return useSelect((select) => select('core/block-editor').hasSelectedInnerBlock(clientId, true));
 }
