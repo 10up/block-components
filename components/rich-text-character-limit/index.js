bcdb61fea29b1b01220d754a197c911dd2478f37
Fabian Kägy <mail@fabian-kaegy.de>
2022-09-08 16:28:29 +0200

    add icons to indicate state of counter

--- components/rich-text-character-limit/index.js
+++ components/rich-text-character-limit/index.js
@@ -29,6 +29,7 @@ const getCharacterCount = (str) => {
  */
 const Counter = forwardRef((props, ref) => {
 	const { count, limit } = props;
+	const percentage = (count / limit) * 100;
 	return (
 		<StyledCounter
 			className={cx('tenup--block-components__character-count', {
@@ -41,7 +42,7 @@ const Counter = forwardRef((props, ref) => {
 				<span className="tenup--block-components__character-count__count">{count}</span> /{' '}
 				<span className="tenup--block-components__character-count__limit">{limit}</span>
 			</div>
-			<CircularProgressBar percentage={(count / limit) * 100} />
+			<CircularProgressBar percentage={percentage} />
 		</StyledCounter>
 	);
 });
@@ -54,6 +55,10 @@ const StyledSvg = styled('svg')`
 		stroke: #fff;
 		stroke-width: 1em;
 		opacity: 0.3;
+	}
+
+	& path {
+		fill: #fff;
 	}
 
 	& .bar {
@@ -79,6 +84,9 @@ const CircularProgressBar = (props) => {
 
 	const strokeDashoffset = ((100 - normalizedPercentage) / 100) * circumference;
 
+	const isApproachingLimit = percentage > 80;
+	const isOverLimit = percentage >= 100;
+
 	return (
 		<StyledSvg
 			xmlns="http://www.w3.org/2000/svg"
@@ -103,6 +111,24 @@ const CircularProgressBar = (props) => {
 				strokeDasharray={circumference}
 				strokeDashoffset={strokeDashoffset}
 			/>
+			{isApproachingLimit && !isOverLimit && (
+				<path
+					style={{ transform: 'rotate(90deg)', transformOrigin: 'center' }}
+					d="M100,31.2c38,0,68.8,30.8,68.8,68.8S138,168.8,100,168.8S31.2,138,31.2,100S62,31.2,100,31.2z M109.7,111.9
+				l3-55.6H87.3l3,55.6C90.3,111.9,109.7,111.9,109.7,111.9z M108.9,140.8c2.1-2,3.2-4.7,3.2-8.3c0-3.6-1-6.4-3.1-8.3
+				c-2.1-2-5.1-3-9.1-3c-4,0-7.1,1-9.2,3c-2.1,2-3.2,4.7-3.2,8.3c0,3.5,1.1,6.3,3.3,8.3c2.2,2,5.2,2.9,9.1,2.9S106.8,142.7,108.9,140.8
+				z"
+				/>
+			)}
+			{isOverLimit && (
+				<path
+					style={{ transform: 'rotate(90deg)', transformOrigin: 'center' }}
+					d="M100,168.8c38,0,68.8-30.8,68.8-68.8c0-38-30.8-68.8-68.8-68.8C62,31.2,31.2,62,31.2,100
+				C31.2,138,62,168.8,100,168.8z M127,73c2.2,2.2,2.2,5.9,0,8.1L108.1,100l18.9,18.9c2.2,2.2,2.2,5.9,0,8.1c-2.2,2.2-5.9,2.2-8.1,0
+				L100,108.1L81.1,127c-2.2,2.2-5.9,2.2-8.1,0c-2.2-2.2-2.2-5.9,0-8.1L91.9,100L73,81.1c-2.2-2.2-2.2-5.9,0-8.1s5.9-2.2,8.1,0
+				L100,91.9L118.9,73C121.1,70.8,124.7,70.8,127,73z"
+				/>
+			)}
 		</StyledSvg>
 	);
 };
