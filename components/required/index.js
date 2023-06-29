31c5acbd974760b0fa03a26dfba278be1baaaf5c
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2023-06-29 19:19:48 +0530

    remove unnecessary store

--- components/required/index.js
+++ components/required/index.js
@@ -1,36 +1,23 @@
 import PropTypes from 'prop-types';
-import { useDispatch, useSelect } from '@wordpress/data';
+import { useDispatch } from '@wordpress/data';
 import { useEffect, useRef } from '@wordpress/element';
 import { v4 as uuidv4 } from 'uuid';
 import { store as editorStore } from '@wordpress/editor';
-import { store as requiredFieldsStore } from './store';
 
-const lockKey = 'tenup-required-fields-lock-post'
 
 export const Required = ({ value, children }) => {
 	const { current: componentId } = useRef( uuidv4() );
 	const { lockPostSaving, unlockPostSaving } = useDispatch( editorStore );
-	const { setIsFilled } = useDispatch( requiredFieldsStore );
 
 	useEffect( () => {
-		setIsFilled( componentId, !! value );
-	}, [ value ] );
-
-	const { shouldLock } = useSelect( ( select ) => {
-		return {
-			shouldLock: select( requiredFieldsStore ).shouldLock(),
-		}
-	} );
-
-	useEffect( () => {
-		if ( shouldLock ) {
-			lockPostSaving( lockKey );
+		if ( ! value ) {
+			lockPostSaving( componentId );
 		} else {
-			unlockPostSaving( lockKey );
+			unlockPostSaving( componentId );
 		}
 
-		return () => unlockPostSaving( lockKey );
-	}, [ shouldLock ] );
+		return () => unlockPostSaving( componentId );
+	}, [ value ] );
 
 	return children;
 };
