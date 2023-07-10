18cfaffe5b460735a935720eb285c8af4e8b10e3
Fabian Kägy <mail@fabian-kaegy.de>
2023-07-10 14:02:20 +0200

    fix remove invalid optional chaining

--- components/icon-picker/inline-icon-picker.tsx
+++ components/icon-picker/inline-icon-picker.tsx
@@ -22,7 +22,7 @@ export const InlineIconPicker: FC<IconPickerProps> = (props) => {
 	const { value, ...rest } = props;
 	const IconButton = useCallback(
 		({ onToggle }: RenderToggleProps) => (
-			<Icon name={value.name} iconSet={value?.iconSet} onClick={onToggle} {...rest} />
+			<Icon name={value.name} iconSet={value.iconSet} onClick={onToggle} {...rest} />
 		),
 		[value, rest],
 	);
