b172c199e65613f784e0320073df60e59f8eb73e
Neil Coetzer <neil@gapshift.com>
2023-07-17 12:37:20 +0200

    Added linebreaks for attributes/props to multi-image example edit file

--- example/src/blocks/multiple-image-example/edit.js
+++ example/src/blocks/multiple-image-example/edit.js
@@ -15,11 +15,36 @@ export function BlockEdit(props) {
     return (
         <div {...blockProps}>
 
-            <Image id={image1} size="large" onSelect={(image) => setAttributes({image1: image.id })} className="example-image" focalPoint={focalPoint1} onChangeFocalPoint={(value) => setAttributes({focalPoint1: value})} hasInlineControls={true} onRemove={() => setAttributes({image1: null})} isOptional={false} />
+            <Image
+            id={image1}
+            size="large"
+            onSelect={(image) => setAttributes({image1: image.id })}
+            className="example-image"
+            focalPoint={focalPoint1}
+            onChangeFocalPoint={(value) => setAttributes({focalPoint1: value})}
+            hasInlineControls={true} onRemove={() => setAttributes({image1: null})}
+            isOptional={false}
+            />
 
-            <Image id={image2} size="large" onSelect={(image) => setAttributes({image2: image.id })} className="example-image" focalPoint={focalPoint2} onChangeFocalPoint={(value) => setAttributes({focalPoint2: value})} hasInlineControls={true} onRemove={() => setAttributes({image2: null})} />
+            <Image
+            id={image2}
+            size="large"
+            onSelect={(image) => setAttributes({image2: image.id })}
+            className="example-image"
+            focalPoint={focalPoint2}
+            onChangeFocalPoint={(value) => setAttributes({focalPoint2: value})}
+            hasInlineControls={true} onRemove={() => setAttributes({image2: null})}
+            />
 
-            <Image id={image3} size="large" onSelect={(image) => setAttributes({image3: image.id })} className="example-image" focalPoint={focalPoint3} onChangeFocalPoint={(value) => setAttributes({focalPoint3: value})} hasInlineControls={true} onRemove={() => setAttributes({image3: null})} />
+            <Image
+            id={image3}
+            size="large"
+            onSelect={(image) => setAttributes({image3: image.id })}
+            className="example-image"
+            focalPoint={focalPoint3}
+            onChangeFocalPoint={(value) => setAttributes({focalPoint3: value})}
+            hasInlineControls={true} onRemove={() => setAttributes({image3: null})}
+            />
         </div>
     )
 }
