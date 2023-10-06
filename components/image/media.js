a7225b10c1485b4d6b033bed8a1dce2ce1668d78
Neil Coetzer <neil@gapshift.com>
2023-10-06 16:39:53 +0200

    Changes Image.Image component to Media and split into separate component file

---
+++ components/image/media.js
@@ -0,0 +1,78 @@
+import PropTypes from 'prop-types';
+import { MediaPlaceholder, InspectorControls } from '@wordpress/block-editor';
+import { useContext, createContext } from '@wordpress/element';
+import { Spinner, FocalPointPicker, PanelBody, Placeholder } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+export const ImageContext = createContext();
+
+export const Media = (props) => {
+	const { style } = props;
+	const {
+		imageUrl,
+		altText,
+		labels,
+		onSelect,
+		isResolvingMedia,
+		shouldDisplayFocalPointPicker,
+		focalPoint,
+		onChangeFocalPoint,
+		canEditImage,
+		hasImage,
+	} = useContext(ImageContext);
+
+	if (shouldDisplayFocalPointPicker) {
+		const focalPointStyle = {
+			objectFit: 'cover',
+			objectPosition: `${focalPoint.x * 100}% ${focalPoint.y * 100}%`,
+		};
+
+		props.styles = {
+			...style,
+			...focalPointStyle,
+		};
+	}
+
+	if (isResolvingMedia) {
+		return <Spinner />;
+	}
+
+	if (!hasImage && !canEditImage) {
+		return <Placeholder className="block-editor-media-placeholder" withIllustration />;
+	}
+
+	return (
+		<>
+			{shouldDisplayFocalPointPicker && (
+				<InspectorControls>
+					<PanelBody title={__('Image Settings')}>
+						<FocalPointPicker
+							label={__('Focal Point Picker')}
+							url={imageUrl}
+							value={focalPoint}
+							onChange={onChangeFocalPoint}
+						/>
+					</PanelBody>
+				</InspectorControls>
+			)}
+			{hasImage && <img src={imageUrl} alt={altText} {...props} />}
+			{canEditImage && (
+				<MediaPlaceholder
+					labels={labels}
+					onSelect={onSelect}
+					accept="image"
+					multiple={false}
+					disableMediaButtons={imageUrl}
+				/>
+			)}
+		</>
+	);
+};
+
+Media.defaultProps = {
+	style: {},
+};
+
+Media.propTypes = {
+	style: PropTypes.object,
+};
