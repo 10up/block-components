dae728e9e3e5c1e6fa625da04376fe033d6bba02
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-03-01 20:55:57 +0530

    remove unnecesary jsdoc comments

--- components/is-admin/index.ts
+++ components/is-admin/index.ts
@@ -1,7 +1,14 @@
 import { useSelect } from '@wordpress/data';
 
 interface IsAdminProps {
+	/**
+	 * Fallback component.
+	 */
 	fallback: React.ReactNode | null;
+
+	/**
+	 * Child component.
+	 */
 	children: React.ReactNode;
 }
 
@@ -11,11 +18,6 @@ interface IsAdminProps {
  * A wrapper component that checks wether the current user has admin capabilities
  * and only returns the child components if the user is an admin. You can pass a
  * fallback component via the fallback prop.
- *
- * @param {object} props react props
- * @param {React.ReactNode|null} props.fallback fallback component
- * @param {React.ReactNode} props.children child components
- * @returns {null|React.ReactNode}
  */
 export const IsAdmin: React.FC<IsAdminProps> = ({
 	fallback = null,
