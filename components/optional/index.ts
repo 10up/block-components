1cab61d8f23bf71f61a1c84c8c0eb8279f495f96
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-03-01 20:53:26 +0530

    remove proptypes

--- components/optional/index.ts
+++ components/optional/index.ts
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { useBlockEditContext } from '@wordpress/block-editor';
 
 interface OptionalProps {
@@ -9,16 +8,11 @@ interface OptionalProps {
 export const Optional: React.FC<OptionalProps> = ({
 	value,
 	children,
-}): boolean | React.ReactNode => {
+}) => {
 	const { isSelected } = useBlockEditContext();
 	return (isSelected || !!value) && children;
 };
 
 Optional.defaultProps = {
 	value: '',
-};
-
-Optional.propTypes = {
-	value: PropTypes.string || PropTypes.bool || PropTypes.number,
-	children: PropTypes.node.isRequired,
 };
