a3485d0329107521d194b93dc26abca479d1381f
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-03-01 18:36:36 +0530

    move Optional component to TS

--- components/optional/index.ts
+++ components/optional/index.ts
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
 import { useBlockEditContext } from '@wordpress/block-editor';
 
-export const Optional = ({ value, children }) => {
+interface OptionalProps {
+	value?: string|number|boolean;
+	children: React.ReactNode;
+}
+
+export const Optional: React.FC<OptionalProps> = ({ value, children }): boolean | React.ReactNode => {
 	const { isSelected } = useBlockEditContext();
 	return (isSelected || !!value) && children;
 };
