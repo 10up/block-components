8b862f4c893dd40514dfcd0d0f23a1dbd67cf2b3
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-03-01 19:10:03 +0530

    eslint fix

--- components/optional/index.ts
+++ components/optional/index.ts
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types';
 import { useBlockEditContext } from '@wordpress/block-editor';
 
 interface OptionalProps {
-	value?: string|number|boolean;
+	value?: string | number | boolean;
 	children: React.ReactNode;
 }
 
-export const Optional: React.FC<OptionalProps> = ({ value, children }): boolean | React.ReactNode => {
+export const Optional: React.FC<OptionalProps> = ({
+	value,
+	children,
+}): boolean | React.ReactNode => {
 	const { isSelected } = useBlockEditContext();
 	return (isSelected || !!value) && children;
 };
