8ab4cf5be5c2e912a3d08c19e014c04587be4141
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-03-03 16:03:42 +0530

    add TS support for `PostTitle` component

--- components/post-title/index.tsx
+++ components/post-title/index.tsx
@@ -1,11 +1,20 @@
 import { useEntityProp } from '@wordpress/core-data';
 import { RichText, store as blockEditorStore } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
-import PropTypes from 'prop-types';
 import { usePost } from '../../hooks';
 
-export const PostTitle = (props) => {
-	const { tagName: TagName = 'h1', ...rest } = props;
+/**
+ * Defines an object type with property `tagName` with type E.
+ * `tagName` is the HTML tag name (e.g., "h1", "a") that can be used in JSX.
+ */
+type PolymorphicTagNameProp<E extends React.ElementType> = {
+	tagName?: E;
+};
+
+type PolymorphicProps<E extends React.ElementType> = React.PropsWithChildren<React.ComponentPropsWithoutRef<E> & PolymorphicTagNameProp<E>>;
+
+const defaultElement = 'h1';
+export function PostTitle<E extends React.ElementType = typeof defaultElement>( { tagName, ...rest }: PolymorphicProps<E> ) {
 	const { postId, postType, isEditable } = usePost();
 
 	const [rawTitle = '', setTitle, fullTitle] = useEntityProp(
@@ -19,6 +28,8 @@ export const PostTitle = (props) => {
 		(select) => select(blockEditorStore).getSettings().titlePlaceholder,
 		[],
 	);
+
+	const TagName = tagName ?? defaultElement;
 
 	if (!isEditable) {
 		// eslint-disable-next-line react/no-danger
@@ -37,10 +48,17 @@ export const PostTitle = (props) => {
 	);
 };
 
-PostTitle.propTypes = {
-	tagName: PropTypes.string,
-};
-
 PostTitle.defaultProps = {
 	tagName: 'h1',
 };
+
+function LOL() {
+	return (
+		<>
+			<PostTitle tagName='h1' id="lol" />
+			<PostTitle tagName='h1' href="lol" />
+			<PostTitle tagName='a' href="lol" />
+			<PostTitle tagName='div' clipPath='what' />
+		</>
+	);
+}
