329341eb9a971b0cf7b9ab048b13100c84419524
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-03-12 10:03:34 +0530

    add undefined type

--- hooks/use-taxonomy/index.ts
+++ hooks/use-taxonomy/index.ts
@@ -13,5 +13,5 @@ export function useTaxonomy(taxonomyName: string) {
 			return [taxonomy, hasResolvedTaxonomy];
 		},
 		[taxonomyName],
-	) as [ Taxonomy, boolean ];
+	) as [ Taxonomy | undefined, boolean ];
 }
