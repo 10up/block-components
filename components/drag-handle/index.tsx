b87fd3a301ec3d0529b134ab0cec86e33f825129
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2025-06-16 01:49:29 +0530

    fix Repeater component dragging issue on WP 6.8

--- components/drag-handle/index.tsx
+++ components/drag-handle/index.tsx
@@ -1,4 +1,4 @@
-import type { SVGProps } from 'react';
+import type { HTMLAttributes } from 'react';
 
 /**
  * Renders an SVG drag handle.
@@ -7,18 +7,19 @@ import type { SVGProps } from 'react';
  * @returns {*} React JSX
  */
 
-export const DragHandle: React.FC<SVGProps<SVGSVGElement>> = (props) => (
-	<svg
-		style={{ marginRight: '10px', cursor: 'grab', flexShrink: 0 }}
-		width="18"
-		height="18"
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 18 18"
-		role="img"
-		aria-hidden="true"
-		focusable="false"
-		{...props}
-	>
-		<path d="M5 4h2V2H5v2zm6-2v2h2V2h-2zm-6 8h2V8H5v2zm6 0h2V8h-2v2zm-6 6h2v-2H5v2zm6 0h2v-2h-2v2z" />
-	</svg>
+export const DragHandle: React.FC<HTMLAttributes<HTMLSpanElement>> = (props) => (
+	<span {...props}>
+		<svg
+			style={{ marginRight: '10px', cursor: 'grab', flexShrink: 0 }}
+			width="18"
+			height="18"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 18 18"
+			role="img"
+			aria-hidden="true"
+			focusable="false"
+		>
+			<path d="M5 4h2V2H5v2zm6-2v2h2V2h-2zm-6 8h2V8H5v2zm6 0h2V8h-2v2zm-6 6h2v-2H5v2zm6 0h2v-2h-2v2z" />
+		</svg>
+	</span>
 );
