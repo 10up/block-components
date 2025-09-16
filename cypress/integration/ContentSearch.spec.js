c07eb7e1d0536cccfbb785cfaee558906e46c6bf
Cory Hughart <cory@alphaparticle.com>
2025-09-16 09:04:43 -0500

    update integration tests

--- cypress/integration/ContentSearch.spec.js
+++ cypress/integration/ContentSearch.spec.js
@@ -14,7 +14,17 @@ context('ContentSearch', () => {
 	it('allows the user to see initial results on focus', () => {
 		cy.createPost({title: 'Post Searcher with fetchOnFocus'});
 		cy.insertBlock('Post Searcher');
-		cy.get('.wp-block-example-content-search .components-text-control__input').focus();
+		cy.get('.wp-block-example-content-search .components-input-control__input').focus();
 		cy.get('.tenup-content-search-list').should('exist');
+	})
+
+	it('displays the post date in the search results', () => {
+		cy.createPost({title: 'Post Searcher with Post Date'});
+		cy.insertBlock('Post Searcher');
+		cy.get('.wp-block-example-content-search .components-input-control__input').focus();
+		cy.get('.tenup-content-search-list .tenup-content-search-list-item time').invoke('attr', 'datetime').then((datetime) => {
+			const date = new Date(datetime);
+			expect(date.toString()).to.not.equal('Invalid Date');
+		});
 	})
 })
